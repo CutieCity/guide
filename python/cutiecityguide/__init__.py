935e70823993419b65fe57db29c70c4bd5516505
nuztalgia <nuztalgia@gmail.com>
2023-04-21 20:22:05 -0700

    Add bundled icons to emoji extension functions

--- python/cutiecityguide/__init__.py
+++ python/cutiecityguide/__init__.py
@@ -20,42 +20,81 @@
 
 """This file contains a custom emoji index and generator to use with pymdownx.emoji."""
 
-from typing import Any
-from xml.etree import ElementTree
+import re
+from collections.abc import Iterator
+from pathlib import Path
+from typing import Any, cast
+from xml.etree import ElementTree as etree
 
 
 def emoji_generator(
-    _index, _shortname, _alias, _uc, alt, title, category, options, _md
-) -> ElementTree.Element:
+    index, shortname, _alias, _uc, alt, title, category, options, md
+) -> etree.Element:
     """Returns an image `Element` object for displaying the requested emoji in HTML."""
-    attributes = {
-        "alt": alt,
-        "class": "cutiemoji",
-        "loading": "lazy",
-        "src": f"https://media.cutie.city/custom_emojis/images/{category}",
-        "title": title or alt,
-    }
+    attributes = {"alt": alt, "title": title or alt}
+
+    if category:
+        attributes |= {
+            "class": "cutiemoji",
+            "loading": "lazy",
+            "src": f"https://media.cutie.city/custom_emojis/images/{category}",
+        }
+        element = etree.Element("img", attributes)
+    else:
+        element = etree.Element("span", attributes | {"class": "twemoji"})
+
+        svg_path = md.inlinePatterns["emoji"].emoji_index["emoji"][shortname]["path"]
+        element.text = md.htmlStash.store(Path(svg_path).read_text())
 
     for key, value in options.get("attributes", {}).items():
-        attributes[key] = value
+        element.set(key, value)
 
-    return ElementTree.Element("img", attributes)
+    return element
 
 
 def emoji_index(_options, _md) -> dict[str, Any]:
     """Returns an index of Cutie City emoji in the format expected by pymdownx.emoji."""
+    from importlib.util import find_spec
     from json import loads
-    from pathlib import Path
 
-    from .custom_emoji import INDEX_FILE_REL_PATH
+    from cutiecityguide.custom_emoji import INDEX_FILE_REL_PATH
 
-    index_path = (Path(__file__) / INDEX_FILE_REL_PATH).resolve()
+    emoji_index_path = Path(__file__) / INDEX_FILE_REL_PATH
+    emoji_index = loads(emoji_index_path.resolve().read_text())
+
+    if material_spec := find_spec("material"):
+        material_directory = Path(cast(str, material_spec.origin)).parent
+
+        for name, path in _get_bundled_icons(material_directory / ".icons"):
+            if name not in emoji_index:
+                emoji_index[name] = {"name": name, "path": path}
 
     return {
         "name": "cutiemoji",
-        "emoji": loads(index_path.read_text()),
+        "emoji": emoji_index,
         "aliases": {},
     }
+
+
+def _get_bundled_icons(source_directory) -> Iterator[tuple[str, Any]]:
+    icon_sets = [
+        (re.compile(f"^:{subdirectory_name}_"), f":{short_name}_")
+        for short_name, subdirectory_name in {
+            "fa": "fontawesome",
+            "md": "material",
+            "oi": "octicons",
+            "si": "simple",
+        }.items()
+    ]
+
+    for absolute_path in source_directory.glob("**/*.svg"):
+        relative_path = absolute_path.relative_to(source_directory)
+        name = f':{relative_path.with_suffix("").as_posix().replace("/", "_")}:'
+
+        for pattern, prefix in icon_sets:
+            name = pattern.sub(prefix, name)
+
+        yield name, str(absolute_path)
 
 
 __all__ = [
